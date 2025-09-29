b10a5a77a4fb31df3097af8a411b1500e3a53083
crimii112 <ycl623@naver.com>
2025-09-29 09:22:52 +0900

    2025.09.29

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from '@/App.jsx';
 import '@/main.css';
-import '@/styles.css';
 
 const queryClient = new QueryClient();
 
